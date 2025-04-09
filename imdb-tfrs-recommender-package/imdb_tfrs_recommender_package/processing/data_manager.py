6ca523028cb175fa131cacfa0ad5711aa855b6fd
chibuikeeugene <chibuikeeugene@gmail.com>
2025-04-09 14:08:53 +0100

    updated the retrieval notebook and started work on the imdb retrieval package

---
+++ imdb-tfrs-recommender-package/imdb_tfrs_recommender_package/processing/data_manager.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import configparser
+
+
+
+def load_dataset():
+    """ load the data from the database or data warehouse. 
+    if none, load just the csv files. """
+    
+    
